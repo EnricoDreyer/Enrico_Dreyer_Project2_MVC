aa658ce9ab4cb48b74ce1f0a518d957c026aa238
Enrico <forty5degreesdev@gmail.com>
2020-10-27 18:37:13 +0200

    Getting started
    Started MVC, Added database

---
+++ Enrico_Dreyer_Project2/Enrico_Dreyer_Project2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Enrico_Dreyer_Project2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
