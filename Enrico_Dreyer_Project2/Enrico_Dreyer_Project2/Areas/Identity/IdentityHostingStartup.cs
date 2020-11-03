e0bbc101c705b77226ffb735bb8efaba4cb19a26
Enrico <forty5degreesdev@gmail.com>
2020-11-03 16:23:57 +0200

    Added Identities

---
+++ Enrico_Dreyer_Project2/Enrico_Dreyer_Project2/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Enrico_Dreyer_Project2.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Enrico_Dreyer_Project2.Areas.Identity.IdentityHostingStartup))]
+namespace Enrico_Dreyer_Project2.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
