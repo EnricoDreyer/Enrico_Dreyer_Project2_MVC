48814e69dec6676c722ecfd69fe17b0c109a04ba
Enrico <forty5degreesdev@gmail.com>
2020-11-17 22:36:11 +0200

    Added Admin checkbox

--- Enrico_Dreyer_Project2/Enrico_Dreyer_Project2/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Enrico_Dreyer_Project2/Enrico_Dreyer_Project2/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -50,8 +50,8 @@ namespace Enrico_Dreyer_Project2.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
-            public bool RememberMe { get; set; }
+            [Display(Name = "Admin")]
+            public bool Admin { get; set; }           
         }
 
         public async Task OnGetAsync(string returnUrl = null)
@@ -79,7 +79,7 @@ namespace Enrico_Dreyer_Project2.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.Admin, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
@@ -87,7 +87,7 @@ namespace Enrico_Dreyer_Project2.Areas.Identity.Pages.Account
                 }
                 if (result.RequiresTwoFactor)
                 {
-                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
+                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.Admin });
                 }
                 if (result.IsLockedOut)
                 {
